eb0c334e85d84c1b77dbd643856eb1de83bddc4f
Felkim <fipojaq2005@gmail.com>
2025-01-15 19:02:53 -0500

    Funciona con groq e implementacion de preguntas

--- app/services/groq_service.py
+++ app/services/groq_service.py
@@ -11,6 +11,28 @@ client = Groq(api_key=GROQ_API_KEY)
 
 def generate_response(message):
     
+    with open(r"C:\Users\felip\OneDrive\Escritorio\BOT_WP\python-whatsapp-bot\data\faq_fotoprinte.txt","r") as file:
+        lectura = file.read()
+        
+        xd = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+            "role":"user",
+            "content": f"Necesito que uses la siguiente informacion: \n {lectura} \n Para que actues como si estuvieras atendiendo a un cliente" 
+            }
+            ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,  
+        )
+        
+        for chunk in xd:
+            print(chunk.choices[0].delta.content or "", end="")
+        
+    
     string_completion = ""
     
     completion = client.chat.completions.create(
